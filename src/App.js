27850a016a4afa4068f11f321ecf6fffdda90252
Samir Talovic <steamzamenabycomi@gmail.com>
2024-04-27 00:41:02 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ function App() {
 
   return (
     <div className="App">
-      
-      <Dmarket/>
+      <Landing/>
     </div>
   );
 }
--- src/pages/Courses/DMarket.js
+++ src/pages/Courses/DMarket.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './Art.css'; // Import the CSS file
 
-const textToRead = "Our Digital Marketing Course is meticulously designed to ignite your passion for online marketing and hone your digital skills. Whether you're new to the realm of digital marketing or a seasoned marketer, our comprehensive curriculum covers a diverse array of topics, including social media marketing, search engine optimization (SEO), content marketing, email marketing, and more. Led by industry experts, you'll receive personalized guidance and hands-on experience to develop effective marketing strategies and leverage digital tools to drive business growth. Join us now and embark on an exciting journey to become a proficient digital marketer.";
+const textToRead = "Our Digital Marketing Course is meticulously designed to";
+
+const initialTextToRead = "Say read me to start.";
 
 const Dmarket = () => {
     const {
@@ -14,13 +16,25 @@ const Dmarket = () => {
     const [isReading, setIsReading] = useState(false);
 
     useEffect(() => {
+        // Call the function to read initial text when the component mounts
+        handleInitialRead();
+    }, []); // Empty dependency array ensures that it runs only once when the component mounts
+
+    useEffect(() => {
         if (isReading) {
             const value = new SpeechSynthesisUtterance(textToRead);
             window.speechSynthesis.speak(value);
+           SpeechRecognition.startListening(); // Start listening for speech recognition
         } else {
             window.speechSynthesis.cancel();
         }
     }, [isReading]);
+
+    // Function to handle the initial speech when the component mounts
+    const handleInitialRead = () => {
+        const initialSpeech = new SpeechSynthesisUtterance(initialTextToRead);
+        window.speechSynthesis.speak(initialSpeech);
+    };
 
     const handleKeyDown = (event) => {
         if (event.keyCode === 32) { // Check if the pressed key is the spacebar
