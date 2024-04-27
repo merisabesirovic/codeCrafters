340dc3de3bb7495ca06ef9d45e55558da33db013
Samir Talovic <steamzamenabycomi@gmail.com>
2024-04-27 02:34:07 +0200

    Audio support for first page

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,13 @@
-import React from "react";
 import Carousel from "react-material-ui-carousel";
 import Item from "./Item";
 import slider from "../slider.json";
 
+
+
 function Example() {
+
   return (
-    <Carousel duration={150} autoPlay={true}>
+    <Carousel  duration={150} autoPlay={true}>
       {slider.map((item) => (
         <Item key={item.id} item={item} />
       ))}
