4475e46aa3e9f278edf396ea75e56652823aea5d
Merisa <besiroviccm@gmail.com>
2024-04-27 05:47:34 +0200

    message

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,13 +10,16 @@ import { useNavigate } from "react-router-dom";
 import nit from "../../img/nit.png";
 import google from "../../img/google.png";
 import uni from "../../img/uninp.png";
+import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice";
+import MicOffIcon from "@mui/icons-material/MicOff";
 const textToRead =
   "Our Digital Marketing Course is meticulously designed to ignite your passion for online marketing and hone your digital skills. Whether you're new to the realm of digital marketing or a seasoned marketer, our comprehensive curriculum covers a diverse array of topics, including social media marketing, search engine optimization (SEO), content marketing, email marketing, and more. Led by industry experts, you'll receive personalized guidance and hands-on experience to develop effective marketing strategies and leverage digital tools to drive business growth. Join us now and embark on an exciting journey to become a proficient digital marketer.";
-const asistentClick = () =>{
-  const text = "If you want to go to art course say art, if you want to go to coding course say code, if you want to go to digital course say digital"
-  const value =  new SpeechSynthesisUtterance(text)
-  window.speechSynthesis.speak(value)
-}
+const asistentClick = () => {
+  const text =
+    "If you want to go to art course say art, if you want to go to coding course say code, if you want to go to digital course say digital";
+  const value = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(value);
+};
 const HomePage = () => {
   const navigate = useNavigate(); // Declare useNavigate here
 
@@ -57,19 +60,31 @@ const HomePage = () => {
 
   if (transcript.toLowerCase() === "art") {
     navigate("/home/art");
-  }else if (transcript.toLowerCase() === "code"){
-    navigate('/home/coding')
-  }else if(transcript.toLowerCase() == "digital"){
-    navigate('/home/digital-marketing')
-  }else if(transcript.toLowerCase() !== "art" || transcript.toLowerCase() !== "code" || transcript.toLowerCase() !== "digital"){
-
+  } else if (transcript.toLowerCase() === "code") {
+    navigate("/home/coding");
+  } else if (transcript.toLowerCase() == "digital") {
+    navigate("/home/digital-marketing");
+  } else if (
+    transcript.toLowerCase() !== "art" ||
+    transcript.toLowerCase() !== "code" ||
+    transcript.toLowerCase() !== "digital"
+  ) {
   }
   return (
     <ThemeProvider theme={theme}>
       <div onClick={asistentClick} className="home-page">
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-<p>{transcript}</p>
+        <div className="f">
+          {listening ? (
+            <KeyboardVoiceIcon color="#1d3455"></KeyboardVoiceIcon>
+          ) : (
+            <MicOffIcon color="#1d3455"></MicOffIcon>
+          )}
+          <p>Microphone: {listening ? "on" : "off"}</p>
+        </div>
+        <Button variant="contained" onClick={SpeechRecognition.startListening}>
+          Start
+        </Button>
+        <p>{transcript}</p>
         <h1></h1>
         <div className="card-container">
           <div className="card">
@@ -91,7 +106,7 @@ const HomePage = () => {
           </div>
           <div className="card">
             <img className="bgimg" src={nit}></img>
-            <h2>UNINP Coding Course</h2>
+            <h2>Centar NIt Coding Course</h2>
             <p>
               The UNINP Coding Course offers comprehensive training in
               programming languages and software development
@@ -106,7 +121,7 @@ const HomePage = () => {
           <div className="card">
             <img className="bgimg" src={google}></img>
 
-            <h2>UNINP Digital Marketing Course</h2>
+            <h2>Google Digital Marketing Course</h2>
             <p>
               UNINP's Digital Marketing Course offers essential skills and
               strategies for success in the digital marketing industry.
