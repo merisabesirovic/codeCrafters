1b2e165fa851e86edaf2c3645ba97e4f7d10ee8e
Merisa <besiroviccm@gmail.com>
2024-04-27 03:55:24 +0200

    message

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,25 +1,27 @@
-// HomePage.js
 import React from "react";
 import "./Home.css"; // Import the CSS file for styling
 import theme from "../../theme/theme";
 import { ThemeProvider } from "@emotion/react";
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import nit from "../../img/nit.png";
+import google from "../../img/google.png";
+import uni from "../../img/uninp.png";
 
 const HomePage = () => {
+  const navigate = useNavigate(); // Declare useNavigate here
+
+  const handleNavigation = (route) => {
+    navigate(route); // Use navigate for navigation
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <div className="home-page">
         <h1></h1>
         <div className="card-container">
           <div className="card">
-            <img
-              className="bgimg"
-              src="https://previews.123rf.com/images/olegdudko/olegdudko1908/olegdudko190803400/128880447-row-of-artist-paint-brushes-on-background.jpg"
-            ></img>
-            <img
-              className="img"
-              src="https://d1hbpr09pwz0sk.cloudfront.net/logo_url/univerzitet-u-novom-pazaru-5eedb1ff"
-            ></img>
+            <img className="bgimg" src={uni}></img>
             <h2>UNINP Art Course</h2>
             <p>
               The UNINP Art Course is a comprehensive program designed to
@@ -27,25 +29,43 @@ const HomePage = () => {
             </p>
 
             <ThemeProvider theme={theme}>
-              <Button variant="contained">Peek at this course</Button>
+              <Button
+                variant="contained"
+                onClick={() => handleNavigation("/home/art")}
+              >
+                Peek at this course
+              </Button>
             </ThemeProvider>
           </div>
           <div className="card">
+            <img className="bgimg" src={nit}></img>
             <h2>UNINP Coding Course</h2>
             <p>
               The UNINP Coding Course offers comprehensive training in
               programming languages and software development
             </p>
-            <Button variant="contained">Peek at this course</Button>
+            <Button
+              variant="contained"
+              onClick={() => handleNavigation("/home/coding")}
+            >
+              Peek at this course
+            </Button>
           </div>
           <div className="card">
+            <img className="bgimg" src={google}></img>
+
             <h2>UNINP Digital Marketing Course</h2>
             <p>
               UNINP's Digital Marketing Course offers essential skills and
               strategies for success in the digital marketing industry.
             </p>
 
-            <Button variant="contained">Peek at this course</Button>
+            <Button
+              variant="contained"
+              onClick={() => handleNavigation("/home/digital-marketing")}
+            >
+              Peek at this course
+            </Button>
           </div>
         </div>
       </div>
